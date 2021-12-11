88f56ccffaafd3d6972faae291f2d3da47693798
ahzia <ah.ziayosfi@gmail.com>
2021-12-11 13:16:46 +0430

    add tests for users#show

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -23,4 +23,27 @@ RSpec.describe "Users", type: :request do
           expect(response.body).to include('Here is a list of all users')
         end
     end
+    describe 'GET /show' do
+        before(:example) { get('/users/300') }
+    
+        it 'check status' do
+          expect(response).to have_http_status(:ok)
+        end
+    
+        it 'renders correct template (Renders spesific user)' do
+          expect(response.body).to render_template('show')
+        end
+    
+        it 'renders correct template (Not Render all Users)' do
+          expect(response).to_not render_template('index')
+        end
+    
+        it 'check for placeholder text' do
+          expect(response.body).to include('Users ID 300')
+        end
+    
+        it 'check for placeholder text' do
+          expect(response.body).to include('Details of the user')
+        end
+      end
 end
