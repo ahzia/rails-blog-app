71e7586a1bd32639733c26d9916c80bff63636ba
ahzia <ah.ziayosfi@gmail.com>
2021-12-11 13:04:03 +0430

    add controller test to gem file

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-
+    describe 'GET /index' do
+        before(:example) { get('/users/300/posts') }
+        it 'check status' do
+          expect(response).to have_http_status(:ok)
+        end
+        it 'renders correct template (All Posts)' do
+            expect(response).to render_template('index')
+        end
+        it 'renders correct template (Not a Specific Post)' do
+            expect(response).to_not render_template('show')
+        end
+        it 'check for placeholder text ' do
+            expect(response.body).to include('Post of User')
+        end
+    end
 end
