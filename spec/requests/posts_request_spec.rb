4f9884cd665410d7827275e822c8f2fa67a4cb34
ahzia <ah.ziayosfi@gmail.com>
2021-12-11 13:10:02 +0430

    add tests for Posts /show

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -16,4 +16,22 @@ RSpec.describe "Posts", type: :request do
             expect(response.body).to include('Post of User')
         end
     end
+    describe 'GET /show' do
+        before(:example) { get('/users/300/posts/20') }
+        it 'check status' do
+          expect(response).to have_http_status(:ok)
+        end
+    
+        it 'renders correct template (Spesific Post)' do
+          expect(response).to render_template('show')
+        end
+    
+        it 'renders correct template (Not all Posts)' do
+          expect(response).to_not render_template('index')
+        end
+    
+        it 'check for placeholder text' do
+          expect(response.body).to include('Post 20 by User')
+        end
+    end
 end
