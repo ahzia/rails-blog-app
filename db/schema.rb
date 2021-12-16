3f68d256e11a4e8552e4a1ffaa7dd7f1c014ba95
ahzia <ah.ziayosfi@gmail.com>
2021-12-16 16:36:20 +0430

    fix issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_15_115625) do
+ActiveRecord::Schema.define(version: 2021_12_16_115013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_12_15_115625) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "author_id"
-    t.bigint "post_id"
+    t.bigint "post_id", null: false
     t.index ["author_id"], name: "index_likes_on_author_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
   end
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2021_12_15_115625) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
-  add_foreign_key "likes", "users", column: "post_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
