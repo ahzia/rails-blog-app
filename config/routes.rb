b62d2266c0b13f2c1a76af08e01a36be1b698ae5
ahzia <ah.ziayosfi@gmail.com>
2021-12-24 23:26:37 +0430

    implement comment delete functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
+    resources :posts do
+      resources :comments
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
